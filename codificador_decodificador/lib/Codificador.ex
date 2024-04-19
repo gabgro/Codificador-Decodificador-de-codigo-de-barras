2648c51a6ff575fee219f0011ffe46e29cc6565e
SamuelMorais <samuelmorais552@gmail.com>
2024-04-19 17:46:56 -0300

    Codificação do fator de validade + testes

--- codificador_decodificador/lib/Codificador.ex
+++ codificador_decodificador/lib/Codificador.ex
@@ -1,4 +1,21 @@
 defmodule Codificador do
+
+
+  def codificar_data_vencimento(data_str) do
+    # Converte em um objeto do tipo Date
+    data_lista = data_str |> String.split("/") |> Enum.map(&String.to_integer/1)
+    data_entrada = Date.new(Enum.at(data_lista,2), Enum.at(data_lista,1), Enum.at(data_lista,0)) |>
+    elem(1)
+    # Data base
+    data_base = Date.new(1997,10,7) |> elem(1)
+    # Calcula o fator de vencimento
+    fator_de_validade = Date.diff(data_entrada, data_base)
+    if (fator_de_validade > 9999) do
+      1000 + rem(fator_de_validade, 1000)
+    else
+      fator_de_validade
+    end
+  end
   @moduledoc """
   Documentation for `CodificadorDecodificador`.
   """
@@ -12,7 +29,9 @@ defmodule Codificador do
       :world
 
   """
-  def hello do
-    :world
-  end
+
+
+
+
+  # def assembler(, dv)
 end
--- codificador_decodificador/test/codificador_decodificador_test.exs
+++ codificador_decodificador/test/codificador_decodificador_test.exs
@@ -1,8 +1,20 @@
-defmodule CodificadorDecodificadorTest do
+defmodule CodificadorTest do
   use ExUnit.Case
-  doctest CodificadorDecodificador
+  doctest Codificador
 
-  test "greets the world" do
-    assert CodificadorDecodificador.hello() == :world
+  test "Fator de Vencimento 1" do
+    assert Codificador.codificar_data_vencimento("04/07/2000") == 1001
+  end
+  test "Fator de Vencimento 2" do
+  assert Codificador.codificar_data_vencimento("01/05/2002") == 1667
+  end
+  test "Fator de Vencimento 3" do
+  assert Codificador.codificar_data_vencimento("21/02/2025") == 9999
+  end
+  test "Fator de Vencimento 4" do
+  assert Codificador.codificar_data_vencimento("22/02/2025") == 1000
+  end
+  test "Fator de Vencimento 5" do
+  assert Codificador.codificar_data_vencimento("23/02/2025") == 1001
   end
 end
