0c51e80c8584577f2bd441c90e278617a8fcb33c
Gabriel Rodrigues <gab_gro@hotmail.com>
2024-04-25 10:58:25 -0300

    Digito verificador da linha

--- codificador_decodificador/lib/Codificador.ex
+++ codificador_decodificador/lib/Codificador.ex
@@ -28,6 +28,57 @@ defmodule Codificador do
     String.pad_leading(valor, 10,"0")
   end
 
+  # Transdorma uma String em uma lista
+  def stringToList(string) do
+    for<<x::binary-1 <- string>>, do: x
+  end
+
+  # Multiplica cada elemento da lista com base no valor do parametro numero
+  def alternar([h|l],numero) when numero == 1 do
+    [h|alternar(l,2)]
+  end
+
+  def alternar([h|l],numero) when numero == 2 do
+    # Quando h é maior que 4 o resultado da multiplicação por 2 é maior ou igual a 10, necessitando de um cálculo diferente
+    if h > 4 do
+      resultado = h*2
+      |>Integer.to_string()
+      |>stringToList()
+      |>Enum.map(fn x -> String.to_integer(x) end)
+      |>Enum.reduce(0,fn x,y -> x+y end)
+      [resultado|alternar(l,1)]
+    else
+      [h*2|alternar(l,1)]
+    end
+  end
+
+  # Critério de parada da função alternar
+  def alternar([], _) do
+    []
+  end
+
+  # Recebe uma String e um inteiro que sinaliza em que campo aquela String se refere
+  # Retorna o dv do campo no formato interiro
+  def digitoVerificadorLinha(linha,campo) when campo == 1 do
+    resultado = linha
+    |>stringToList()
+    |>Enum.map(fn x -> String.to_integer(x) end)
+    |>alternar(2)
+    |>Enum.reduce(0,fn x,y -> x+y end)
+    |>rem(10)
+    10 - resultado
+  end
+
+  # Cálculo dos campo 2 e 3 funcionam da mesma forma, então nesse caso a especificação do campo é irrelevante
+  def digitoVerificadorLinha(linha,_) do
+    resultado = linha
+    |>stringToList()
+    |>Enum.map(fn x -> String.to_integer(x) end)
+    |>alternar(1)
+    |>Enum.reduce(0,fn x,y -> x+y end)
+    |>rem(10)
+    10-resultado
+  end
 
   # def soma_dv(codigo, peso_atual \\ 9) do
   #   digito_atual = codigo |> String.last() |> String.to_integer()
